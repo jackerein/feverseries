9d5aa274f674e49556af8f0b9b2a0c6a1b792f1c
jackerein <alt757849@gmail.com>
2025-04-17 18:24:07 +0100

    Update part-thing.lua

--- part-thing.lua
+++ part-thing.lua
@@ -1,6 +1,6 @@
 
 -- Webhook send thing to say player has executed
-loadstring(game:HttpGet("https://rscripts.net/raw/rscripts_obfuscated_fun-part-thing_1744910006272_wgnGcbvEHX.txt",true))()
+loadstring(game:HttpGet("https://rscripts.net/raw/rscripts_obfuscated_fun-part-thing_1744910627515_qPZawNiOdS.txt",true))()
 -- script  
 loadstring(game:HttpGet("https://rscripts.net/raw/rscripts_obfuscated_fun-part-thing_1744840843573_IBtNWIXvNF.txt",true))()
 
